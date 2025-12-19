51e1a47982bb8df045cae6c3a0926763d4bf9e50
X <den2007fad@gmail.com>
2025-12-19 09:40:59 +0300

    feat: add CORS

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,16 @@ require('./config/passport.config');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const cors = require('cors');
+
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 const authRoutes = require('./routes/auth.routes');
 const airportRoutes = require('./routes/airport.routes');
